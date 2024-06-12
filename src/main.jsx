b67aaa857231c082259b4a9ec3c61bcf2f22f397
Felipe Arce <arceprogramando@gmail.com>
2024-06-12 14:55:34 -0300

    feat : move env to git ignore

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAkExgyBcS_-cavdKTXz5CA2Pz3Tu3vA3Q",
-  authDomain: "zaragoza-shoes-c44ec.firebaseapp.com",
-  projectId: "zaragoza-shoes-c44ec",
-  storageBucket: "zaragoza-shoes-c44ec.appspot.com",
-  messagingSenderId: "732896996580",
-  appId: "1:732896996580:web:8e21431e1b6a74181e50f1"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
